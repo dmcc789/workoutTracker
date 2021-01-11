bd4434c8e89ea30e79cde00c5de722a08efce3b4
Deirdre Cruice <dmcruice@gmail.com>
2021-01-10 23:52:51 -0500

    server js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
@@ -13,15 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/html-routes.js"));
